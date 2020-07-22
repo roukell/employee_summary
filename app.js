3b785e120c1ba3d038672ca7ceb53b9045d001d7
roukell <62136980+roukell@users.noreply.github.com>
2020-07-22 10:54:29 +1000

    added employee, manager, engineer, intern constructor

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 var prompt = inquirer.createPromptModule();
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // array of questions for user
 const managerQuestion = {
