98069dace57655ef7fd3ce14d94470ba3af919bd
roukell <62136980+roukell@users.noreply.github.com>
2020-07-22 11:06:07 +1000

    change var to const line 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-var prompt = inquirer.createPromptModule();
+const prompt = inquirer.createPromptModule();
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
